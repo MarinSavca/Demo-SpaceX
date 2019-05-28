a0f5280287baa81f97d7d4853d276f265aadf46c
Savca Marin <savca.a.marin@gmail.com>
2019-05-28 21:47:20 +0300

    Minor update.

--- Space X/Controllers/DetailController.swift
+++ Space X/Controllers/DetailController.swift
@@ -13,25 +13,28 @@ class LaunchDetailController: UIViewController {
    
    var launch: Launch!
    
+   // MARK: - Private parameters
    @IBOutlet weak private var dateLabel: UILabel!
    @IBOutlet weak private var descriptionLabel: UILabel!
    @IBOutlet weak private var nameLabel: UILabel!
    @IBOutlet weak private var massLabel: UILabel!
-   @IBOutlet weak private var webView: WKWebView!
+   @IBOutlet weak var webView: WKWebView!
    
    private let viewModel = LaunchDetailControllerViewModel()
    
+   // MARK: - Lifecycle
    override func viewDidLoad() {
       super.viewDidLoad()
-      
       viewModel.launch = launch
       configureMandatoryLabels()
       configureOptionalLabels()
       configureWebView()
    }
    
+   // MARK: - Setup
    private func configureMandatoryLabels() {
       DispatchQueue.main.async {
+         self.title = self.launch.missionName
          self.dateLabel.text = self.viewModel.date
          self.nameLabel.text = self.viewModel.name
          self.massLabel.text = self.viewModel.payloadMass
@@ -48,16 +51,15 @@ class LaunchDetailController: UIViewController {
    
    private func configureWebView() {
       webView.allowsLinkPreview = true
+      webView.isOpaque = false
+      webView.backgroundColor = UIColor(red: 23/255, green: 25/255, blue: 31/255, alpha: 1)
       if let request = viewModel.youtubeRequest {
          webView.load(request)
       }
    }
-}
-
-extension LaunchDetailController {
    
+   // MARK: - Actions
    @IBAction private func openWiki(_ sender: UIButton) {
       UIApplication.shared.open(viewModel.wikiURL, options: [:])
    }
-   
 }
